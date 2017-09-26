477456131449470db0a489b518a6d803f9082ed8
Simo Asni <=>
2017-09-26 13:54:15 -0400

    first commit;

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/register', function (req, res) {
+	// register post api call
+});
+
+router.post('/login', function (req, res) {
+	// login post api call
+});
+
+module.exports = router;
